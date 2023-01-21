8bffb12e7911475ff2be0487933f3d86d9bb08a8
Ashish Gupta <ashish.gupta@nobroker.in>
2023-01-21 18:58:16 +0530

    completed

--- app/src/main/java/com/example/eztap/ui/MainActivity.kt
+++ app/src/main/java/com/example/eztap/ui/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.example.eztap.ui
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ProgressBar
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
@@ -17,17 +16,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
-import androidx.compose.ui.unit.sp
 import com.example.datalayyer.model.Uidata
 import com.example.eztap.R
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+import kotlin.collections.HashMap
 
 
 @AndroidEntryPoint
@@ -54,7 +49,13 @@ class MainActivity : ComponentActivity() {
 
         var loadingState =  viewModel.dataLoading.observeAsState()
 
+
         Box(modifier = Modifier.fillMaxSize()){
+
+
+            var modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.Center);
 
             if(errorState.value!!.first) {
 
@@ -68,114 +69,41 @@ class MainActivity : ComponentActivity() {
 
             if(loadingState.value!!) {
 
-                CircularProgressIndicator(
-                    // below line is use to add padding
-                    // to our progress bar.
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .align(Alignment.Center),
 
-                    // below line is use to add color
-                    // to our progress bar.
-                    color = colorResource(id = R.color.purple_200),
-
-                    // below line is use to add stroke
-                    // width to our progress bar.
-                    strokeWidth = Dp(value = 4F)
-                )
+                LoadingBar(modifier = modifier)
 
             }
 
-            LazyColumn(
 
-                modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(16.dp)
+            uiDataList.value?.let {
 
-            ) {
+                Log.d("AshishGupta","0")
 
-                uiDataList.value?.let {listData->
-                    items(listData) { data ->
-                        CustomItem(itemData = data)
-                    }
-                }
+                ReusableComposable.LazyColumnView(uiDataList = it)
+
+                NextActivityButton(it)
 
             }
+
 
         }
     }
 
-
     @Composable
-    fun CustomItem(itemData:Uidata){
+    fun NextActivityButton(uiDataList:List<Uidata>){
 
-        var input by remember { mutableStateOf("") }
 
-        var textStyle = TextStyle(
-            color = Color.Black,
-            fontSize = 16.sp,
-            fontFamily = FontFamily.Monospace,
-            fontWeight = FontWeight.W800,
-            fontStyle = FontStyle.Normal,
-            letterSpacing = 0.5.em,
-        )
-
-        when(itemData.uitype){
-
-            "label"->{
-
-                CustomTextLabel(itemData = itemData, textStyle=textStyle)
-
-            }
-            "edittext"->{
-
-                CustomTextField(itemData = itemData)
-
-            }
-            "button"->{
-
-                CustomButton(itemData = itemData, textStyle = textStyle)
-
-            }
-        }
-    }
-
-    @Composable
-    fun CustomTextLabel(itemData: Uidata, textStyle: TextStyle) {
-
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 5.dp),
-            text = itemData.value!!,
-            style = textStyle
-        )
-
-    }
-
-    @Composable
-    fun CustomTextField(itemData:Uidata){
-
-        var input by remember { mutableStateOf("") }
-        TextField(
-            value = input,
-            modifier = Modifier.fillMaxWidth(),
-            onValueChange = { input = it },
-            placeholder = {
-                Text(itemData.hint!!)
-            },
-        )
-    }
-
-    @Composable
-    fun CustomButton(itemData:Uidata, textStyle: TextStyle){
+        Log.d("AshishGupta","1")
 
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
+
             Button(
                 onClick = {
-                    //Navigate to next scree
+                    Log.d("AshishGupta","100")
+                    gotToSecondActivity(uiDataList)
                 },
                 modifier = Modifier.padding(all = 10.dp),
                 enabled = true,
@@ -184,9 +112,45 @@ class MainActivity : ComponentActivity() {
 
                 )
             {
-                Text(text = itemData.value!!, color = Color.White, style = textStyle)
+                Text(text = "Go to Next Activity")
             }
         }
+
+    }
+
+    fun gotToSecondActivity(uiData:List<Uidata>){
+
+        var intent = Intent(this@MainActivity, SecondActivity::class.java);
+
+        for(i in 0..uiData.size-1){
+
+            uiData[i].key?.let {
+
+                intent.putExtra(it, uiData[i])
+
+            }
+
+        }
+
+        startActivity(intent)
+    }
+
+    @Composable
+    fun LoadingBar(modifier: Modifier) {
+
+        CircularProgressIndicator(
+            // below line is use to add padding
+            // to our progress bar.
+            modifier = modifier,
+
+            // below line is use to add color
+            // to our progress bar.
+            color = colorResource(id = R.color.purple_200),
+
+            // below line is use to add stroke
+            // width to our progress bar.
+            strokeWidth = Dp(value = 4F)
+        )
 
     }
 }
--- app/src/main/java/com/example/eztap/ui/SecondActivity.kt
+++ app/src/main/java/com/example/eztap/ui/SecondActivity.kt
@@ -2,12 +2,46 @@ package com.example.eztap.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.example.datalayyer.model.Uidata
 import com.example.eztap.R
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.TreeMap
 
 @AndroidEntryPoint
 class SecondActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val bundle = intent.extras;
+
+        if(bundle != null){
+
+            var uiData = mutableListOf<Uidata>()
+
+            var keys = bundle.keySet();
+
+            for(key in keys){
+
+                uiData.add(bundle.get(key) as Uidata)
+
+            }
+
+            setContent {
+
+                Box(modifier = Modifier.fillMaxSize()){
+
+                    ReusableComposable.LazyColumnView(uiDataList = uiData)
+                }
+
+            }
+
+        }
     }
 }
