fb44dfaed7594b497413f58b38efa0a6af8e1e1d
Ashish Gupta <ashish.gupta@nobroker.in>
2023-01-21 16:57:33 +0530

    added progress bar

--- app/src/main/java/com/example/eztap/ui/MainActivity.kt
+++ app/src/main/java/com/example/eztap/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.eztap.ui
 
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -8,29 +9,26 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
-import com.example.datalayyer.model.Uidata
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
-import androidx.fragment.app.activityViewModels
-import com.example.datalayyer.model.UiResponse
+import com.example.datalayyer.model.Uidata
+import com.example.eztap.R
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -56,49 +54,47 @@ class MainActivity : ComponentActivity() {
 
         var loadingState =  viewModel.dataLoading.observeAsState()
 
-        LazyColumn(
+        Box(modifier = Modifier.fillMaxSize()){
 
-            modifier = Modifier.fillMaxWidth(),
-            contentPadding = PaddingValues(16.dp)
+            if(errorState.value!!.first) {
 
-        ) {
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Center),
+                    text = errorState.value!!.second
+                )
 
-            uiDataList.value?.let {listData->
-                items(listData) { data ->
-                    CustomItem(itemData = data)
-                }
             }
 
-            item {
+            if(loadingState.value!!) {
 
-                if(errorState.value!!.first) {
+                CircularProgressIndicator(
+                    // below line is use to add padding
+                    // to our progress bar.
+                    modifier = Modifier.padding(16.dp).align(Alignment.Center),
 
-                    Box(
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        Text(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .align(Alignment.Center),
-                            text = errorState.value!!.second
-                        )
+                    // below line is use to add color
+                    // to our progress bar.
+                    color = colorResource(id = R.color.purple_200),
+
+                    // below line is use to add stroke
+                    // width to our progress bar.
+                    strokeWidth = Dp(value = 4F)
+                )
+
+            }
+
+            LazyColumn(
+
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(16.dp)
+
+            ) {
+
+                uiDataList.value?.let {listData->
+                    items(listData) { data ->
+                        CustomItem(itemData = data)
                     }
-
-                }
-
-                if(loadingState.value!!) {
-
-                    Box(
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        Text(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .align(Alignment.Center),
-                            text = "Loading...."
-                        )
-                    }
-
                 }
 
             }
